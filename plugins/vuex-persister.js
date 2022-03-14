dc009f0070d1857dbca12322117cd98c38b05ea8
shadrqen <shadrqen@outlook.com>
2022-03-14 13:42:30 +0300

    feat: add sessionStorage

--- plugins/vuex-persister.js
+++ plugins/vuex-persister.js
@@ -1,17 +1,7 @@
 import VuexPersistor from 'vuex-persister'
-import SecureLS from 'secure-ls'
-
-const SecureLocalStorage = new SecureLS({ encodingType: 'aes' }) // AES encryption and data compression
 
 export default ({ store }) => {
   new VuexPersistor({
-    storage: {
-      getItem: key => SecureLocalStorage.get(key),
-      setItem: (key, value) => SecureLocalStorage.set(key, value),
-      removeItem: key => SecureLocalStorage.remove(key),
-      length: SecureLocalStorage.getAllKeys().length,
-      clear: () => SecureLocalStorage.clear(),
-      key: () => null
-    }
+    storage: sessionStorage
   }).persist(store)
 }
